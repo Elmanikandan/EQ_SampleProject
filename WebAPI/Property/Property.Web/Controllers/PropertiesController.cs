d2e9c54075bd7f7090405102d9447a7c6563071a
Manikandan E <manikandane6394@gmail.com>
2022-09-21 05:29:44 +0000

    Properties Listing sample

---
+++ WebAPI/Property/Property.Web/Controllers/PropertiesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Property.Web.Models;
+
+namespace Property.Web.Controllers
+{
+    public class PropertiesController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Properties> propertyList = new List<Properties>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44351/api/Properties"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    propertyList = JsonConvert.DeserializeObject<List<Properties>>(apiResponse);
+                }
+            }
+            return View(propertyList);
+        }
+    }
+}
